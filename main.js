6224351b6d2380d6f8a435b6050631770016b1dc
FacunMaldonado <facu.sl94@gmail.com>
2022-06-10 15:58:33 -0300

    falta array fix

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const mejorProm = [];
+
 let pruebaa = document.querySelector('.prueba')
 pruebaa.addEventListener('click', function(){
     n1 = document.getElementById("num1").value;  
@@ -9,8 +11,21 @@ pruebaa.addEventListener('click', function(){
     } else {  
       //si no pongo parseFloat concatenaría n1 con n2 
       suma=parseFloat(n1)+parseFloat(n2)+parseFloat(n3);  
-      prom= suma / 3
+      prom= suma / 3;
     }  
+    mejorProm.push(prom);
     document.getElementById("sumando").innerHTML = 'La nota promedio final es: ' + prom; 
+    mejorPromedio(prom);
+   console.log(mejorProm)
 });
- 
+
+function mejorPromedio (prom){
+  let masAlto = 0;
+  for (i=0 ; i < mejorProm.length ;i++){
+    if (masAlto > prom){
+      masAlto = prom;
+    }
+  }
+  document.getElementById("sumando2").innerHTML = 'El mejor promedio ingresado al momento es: ' + masAlto;
+}
+
